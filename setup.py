a6fdff8b5e5f13220636c00fc293b4208d3acef9
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-12-17 02:02:06 +0100

    Add setup.py and requirements file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Pyquantum',
+      version='1.0',
+      description='Quantum computing circuits, gates and example algorithms',
+      author='Miguel Flores Ruiz de Eguino',
+      author_email='miguel.frde@gmail.com',
+      url='https://www.github.com/miguelfrde/pyquantum',
+      packages=['pyquantum'],
+)
