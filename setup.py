d5d5ee91fea891fb8b212ae542a2d52464f49494
Miguel <1889916+miguelfrde@users.noreply.github.com>
2020-04-25 21:41:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='Pyquantum',
       version='1.0',
       description='Quantum computing circuits, gates and example algorithms',
-      author='Miguel Flores Ruiz de Eguino',
+      author='Miguel Flores',
       url='https://www.github.com/miguelfrde/pyquantum',
       packages=['pyquantum'],
 )
