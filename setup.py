d5c705088a6ec2d49c89f0a291a239b069141213
Miguel <1889916+miguelfrde@users.noreply.github.com>
2020-04-25 21:41:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(name='Pyquantum',
       version='1.0',
       description='Quantum computing circuits, gates and example algorithms',
       author='Miguel Flores Ruiz de Eguino',
-      author_email='miguel.frde@gmail.com',
       url='https://www.github.com/miguelfrde/pyquantum',
       packages=['pyquantum'],
 )
